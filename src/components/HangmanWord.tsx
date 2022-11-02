978e78bb4e67bd84b88040beca045faeed1d60ef
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-11-02 21:29:38 +0100

    implement word& individual letters visibility

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -3,5 +3,30 @@ import {
 } from 'react'
 
 export const HangmanWord: FC = (): ReactElement => {
-	return <></>
+	const word = "test"
+	const guessedLetters = ["t"]
+	return <section style={{
+		display: "flex",
+		gap: ".25em",
+		fontSize: "6rem",
+		fontWeight: "bold",
+		textTransform: "uppercase",
+		fontFamily: "monospace"
+	}}>
+		<p>
+			{word.split("").map((letter, index) => (
+				<span key={index} style={{
+					borderBottom: ".1em solid white"
+				}}>
+					<span style={{
+						visibility: guessedLetters.includes(letter)
+							? "visible"
+							: "hidden"
+					}}>
+						{letter}
+					</span>
+				</span>
+			))}
+		</p>
+	</section>
 }
